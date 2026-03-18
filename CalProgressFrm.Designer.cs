93d9b463399008ec13d89ef1d28991121e06d41f
silent-jay <jeremy.martin984@outlook.com>
2026-03-17 22:38:48 -0500

    homework

--- CalProgressFrm.Designer.cs
+++ CalProgressFrm.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // CalProgressFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1736, 1052);
+            this.Name = "CalProgressFrm";
             this.Text = "CalProgressFrm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- CalProgressFrm.cs
+++ CalProgressFrm.cs
@@ -16,6 +16,8 @@ namespace Rotronic
         {
             InitializeComponent();
 
+            this.FormClosed += CalProgressFrm_FormClosed;
+
             /*
              * General plan for calibration procedures and UI/UX features to implement:
              * 
@@ -35,6 +37,37 @@ namespace Rotronic
              * 
              * 
              */
+            
+        }
+
+        private void CalProgressFrm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SafeClose();
+        }
+        public void ChamberController(Chamber chamber, bool Manual, StepClass stepClass)
+        {
+            if (Manual)
+            {
+                MessageBox.Show("Please set the chamber to the following conditions: " + stepClass.SetPointTemp + "°C and " + stepClass.SetPointRH + "%RH. Click OK when conditions are stable.");
+            }
+            else
+            {
+                ChamberCommands.SetTempSP(chamber, stepClass.SetPointTemp);
+                ChamberCommands.SetHumSP(chamber, stepClass.SetPointRH);
+                ChamberCommands.SetTempControl(chamber, true);
+                ChamberCommands.SetRHControl(chamber, true);
+                // Monitor stability and begin soak time countdown when stable
+                while (!chamber.TempStable || !chamber.HumStable)
+                {
+                    //TODO: Add timeout in case chamber cannot reach conditions - 0°C and 5%RH are difficult conditions, need a "close enough" option
+                    Application.DoEvents(); // Keep UI responsive
+                }
+            }
+        }
+        public void SafeClose()
+        {
+            ChamberCommands.SetRHControl(SelectedChamber, false);
+            ChamberCommands.SetTempControl(SelectedChamber, false);
         }
     }
 }
